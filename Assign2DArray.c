2d0cd0c982e51f2fbb5947339bc9394f514549e0
Dkp21 <dhruval.patel2@gmail.com>
2020-08-01 12:19:05 -0400

    Added a second method for creating 2D array

--- Assign2DArray.c
+++ Assign2DArray.c
@@ -22,6 +22,24 @@
 const int ROW = 3;
 const int COL = 4;
 
+// row 0 - > 1 1 1
+// row 1 - > 2 2 2
+// row 3 - > 3 3 3
+
+// Row and column are allocated in contigious memory
+int **assign2DArrayMethod2(int row, int col)
+{
+    int **arr2D = (int **)malloc(sizeof(int*)*row + (row *col));
+
+    for(int i = 0; i < row; ++i)
+    {
+        arr2D[i] = (int *)((int*)arr2D + ((sizeof(int*)*row) + (i * col)));
+    }
+
+    return arr2D;
+}
+
+// Row and column are allocated in non-contigious memory
 int **assign2DArrayMethod1(int row, int col)
 {
     int **arr2D = (int **)malloc(sizeof(int *)*row);
@@ -34,22 +52,65 @@ int **assign2DArrayMethod1(int row, int col)
     return arr2D;
 }
 
-
-int main(int argc, char ** argv)
+// Memory allocated using assign2DArrayMethod1() needs
+// special method to deallocate assigned memory
+void free2DArrayMethod1(int **arr2D, int row)
 {
-    int **arr2D = assign2DArrayMethod1(ROW, COL);
-
-    for(int i = 0; i < ROW; ++i)
+    for(int i = 0; i < row; ++i)
     {
-        for(int j = 0; j < COL; ++j)
+        free(arr2D[i]);
+    }
+
+    free(arr2D);
+}
+
+void initialize2DArrayAndPrint(int **arr2D, int row, int col)
+{
+    for(int i = 0; i < row; ++i)
+    {
+        for(int j = 0; j < col; ++j)
         {
             arr2D[i][j] = i + j;
 
-            printf("%d ", arr2D[i][j]);
+            printf("%3d ", arr2D[i][j]);
         }
 
         printf("\n");
     }
+}
+
+int main(int argc, char ** argv)
+{
+    int row;
+    int col;
+
+    printf("Enter number of row : ");
+
+    scanf("%d", &row);
+
+    printf("Enter number of col : ");
+
+    scanf("%d", &col);
+
+    printf("++++++++++++ %d x %d Matrix using method1 +++++++++++\n", row, col);
+
+    int **arr2D = assign2DArrayMethod1(row, col);
+
+    initialize2DArrayAndPrint(arr2D, row, col);
+
+    // Needs special API to deallocate memory
+    // allocated using assign2DArrayMethod1()
+    free2DArrayMethod1(arr2D, row);
+
+    printf("++++++++++++ %d x %d Matrix using method2 +++++++++++\n", row, col);
+
+    int **arr2D1 = assign2DArrayMethod2(row, col);
+
+    initialize2DArrayAndPrint(arr2D1, row, col);
+
+    // Memory allocated using assign2DArrayMethod2() 
+    // can freed using standard free
+    free(arr2D1);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # Given an instance delete all instances of an element from linked list
     $ gcc DeleteAllDuplicateElememnt.c
+
+# Create a API to allocate and deallocate 2D array
+    $ gcc Assign2DArray.c
